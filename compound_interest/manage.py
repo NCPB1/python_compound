ee65b20616f5f524d9c08820b9e922d4ac2cfcdc
NCPB1 <naveenchandrapb11@gmail.com>
2025-02-26 14:01:52 +0530

    Update manage.py

--- compound_interest/manage.py
+++ compound_interest/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compound_interest.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'compound_interest.compound_interest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
